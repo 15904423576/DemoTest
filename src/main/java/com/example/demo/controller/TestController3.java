8bfd857d06aeb3d307e04f82f8f4e5da46e0ba4c
hujianxiong <hujianxiong@wecash.net>
2019-01-15 16:34:49 +0800

    回退成功

--- src/main/java/com/example/demo/controller/TestController3.java
+++ src/main/java/com/example/demo/controller/TestController3.java
@@ -2,6 +2,6 @@ package com.example.demo.controller;
 
 public class TestController3 {
     public static void main(String[] args) {
-        System.out.println("回退版本3");
+        System.out.println("回退版本3，成功");
     }
 }
