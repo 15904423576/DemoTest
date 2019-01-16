cbcf5a96e4843a67f0df7e6ff2bc36f2054fba33
hujianxiong <hujianxiong@wecash.net>
2019-01-16 17:38:53 +0800

    Default Changelist1

--- src/main/java/com/example/demo/controller/reflog.java
+++ src/main/java/com/example/demo/controller/reflog.java
@@ -4,7 +4,7 @@ public class reflog {
     public static void main(String[] args) {
         System.out.println("huituixx");
         System.out.println("huituqq");
-
+        System.out.println();
 
     }
 }
