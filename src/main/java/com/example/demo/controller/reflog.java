e7c3887d3f0116c20cc0407cee7c09bc1ad4aff6
hujianxiong <hujianxiong@wecash.net>
2019-01-16 15:17:26 +0800

    huituiqq

--- src/main/java/com/example/demo/controller/reflog.java
+++ src/main/java/com/example/demo/controller/reflog.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 public class reflog {
     public static void main(String[] args) {
-        System.out.println("huitui");
+        System.out.println("huituixx");
+        System.out.println("huituqq");
+
+
     }
 }
