30f23ebf51e69b71bcf7df0251283140cf21baef
hujianxiong <hujianxiong@wecash.net>
2019-01-17 15:12:48 +0800

    第二次

--- src/main/java/com/example/demo/dao/TestDemo.java
+++ src/main/java/com/example/demo/dao/TestDemo.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("第一次尝试");
+        System.out.println("第二次尝试");
     }
 }
 
