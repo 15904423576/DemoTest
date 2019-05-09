662a2c488a0eb31963df1fba111827d4fed832c0
hujianxiong <hujianxiong@wecash.net>
2019-05-09 15:38:37 +0800

    Revert "000132"
    This reverts commit e599f64f

--- src/main/java/com/example/demo/dao/DemoTest.java
+++ src/main/java/com/example/demo/dao/DemoTest.java
@@ -299,7 +299,7 @@ public class DemoTest {
             public void run() {
 
                 System.out.println(System.nanoTime());
-System.out.println();
+
             }
 
         }, 1000, 2000, TimeUnit.MILLISECONDS);
